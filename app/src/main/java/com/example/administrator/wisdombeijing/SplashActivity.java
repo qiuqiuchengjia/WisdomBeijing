eefc4702c2a048c4b1fbedca71b0e89b55c4cafa
qiuqiuchengjia <qiuqiuchengjia@163.com>
2016-06-30 14:15:48 +0800

    添加了一个splash，闪屏页面，欢迎马年的闪屏页面

---
+++ app/src/main/java/com/example/administrator/wisdombeijing/SplashActivity.java
@@ -0,0 +1,51 @@
+package com.example.administrator.wisdombeijing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+
+/**
+ * 闪屏页面
+ */
+public class SplashActivity extends Activity {
+    RelativeLayout rlRoot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        rlRoot = (RelativeLayout) findViewById(R.id.root);
+        startAnim(); //调用动画
+
+    }
+
+    /**
+     * 开始一个动画
+     */
+    private void startAnim() {
+        //设置动画同时运行，动画集合
+        AnimationSet set = new AnimationSet(false);
+        //rotate是一个旋转的动画,从0度旋转360度，围绕图的中心（0.5f和0.5f表示正中心）
+        RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF,
+                0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(1000);//设置旋转动画的时间是一秒
+        rotate.setFillAfter(true);//代表保持动画后的一个状态
+        //设置动画缩放
+        ScaleAnimation scale = new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF,
+                0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scale.setDuration(1000);//设置时间为1秒
+        //渐变，从完全透明到完全不透明
+        AlphaAnimation alpha = new AlphaAnimation(0, 1);
+        alpha.setDuration(1000);
+        alpha.setFillAfter(true);
+        //添加动画进动画集合
+        set.addAnimation(rotate);
+        set.addAnimation(scale);
+        set.addAnimation(alpha);
+    }
+}
