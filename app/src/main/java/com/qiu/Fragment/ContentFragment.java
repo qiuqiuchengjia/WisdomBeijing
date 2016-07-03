e835a507456001542a03237d1fabb73ffd0fcb3e
qiuqiuchengjia <qiuqiuchengjia@163.com>
2016-07-03 23:42:51 +0800

    设置主页bottom默认勾选首页tab

--- app/src/main/java/com/qiu/Fragment/ContentFragment.java
+++ app/src/main/java/com/qiu/Fragment/ContentFragment.java
@@ -1,6 +1,7 @@
 package com.qiu.Fragment;
 
 import android.view.View;
+import android.widget.RadioGroup;
 
 import com.qiu.Activity.R;
 
@@ -11,6 +12,7 @@ import com.qiu.Activity.R;
  * 说明： 主页面内容
  */
 public class ContentFragment extends BaseFragment {
+    private RadioGroup radioGroup;
     /**
      * 初始化布局
      * @return view
@@ -19,6 +21,17 @@ public class ContentFragment extends BaseFragment {
     @Override
     public View initViews() {
         View view=  View.inflate(mActivity, R.layout.fragment_content_menu,null);
+        radioGroup= (RadioGroup) view.findViewById(R.id.rg_group);
         return view;
+    }
+
+    /**
+     * 初始化数据
+     * @author qiu 时间：2016-07-03 23-40
+     */
+    @Override
+    public void initDate() {
+        radioGroup.check(R.id.rb_home);//默认勾选首页
+        super.initDate();
     }
 }
