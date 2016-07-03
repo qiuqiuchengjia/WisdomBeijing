7c154c29ae349de4714c4812e266552fdfd6753d
qiuqiuchengjia <qiuqiuchengjia@163.com>
2016-07-03 22:42:42 +0800

    修改侧边栏预留宽度的注释

--- app/src/main/java/com/qiu/Config/ConfigUI.java
+++ app/src/main/java/com/qiu/Config/ConfigUI.java
@@ -7,5 +7,5 @@ package com.qiu.Config;
  * 说明：用来存放和界面有关的配置
  */
 public class ConfigUI {
-    public static final double SlidingMenuBehindOffset=0.3;//侧边栏预留宽度
+    public static final double SlidingMenuBehindOffset=0.3;//侧边栏预留宽度，采用百分比，100%设置为1
 }
