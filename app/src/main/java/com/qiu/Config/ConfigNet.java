0d3ec5bced47e3024b5d0e29342cfd7a4cac2e02
qiuchengjia <qiuqiuchengjia@163.com>
2016-08-23 15:16:38 +0800

    修改获取新闻的IP地址

--- app/src/main/java/com/qiu/Config/ConfigNet.java
+++ app/src/main/java/com/qiu/Config/ConfigNet.java
@@ -7,6 +7,6 @@ package com.qiu.Config;
  * 说明：网络config，用来存放与网络有关的配置
  */
 public class ConfigNet {
-    public static final String SERVER_URL="http://59.71.5.32";//服务器的URL
+    public static final String SERVER_URL="http://59.71.5.15";//服务器的URL
     public static final String CATEGORIES_URL=SERVER_URL+"/qiu/WisdomBeijingServer/categories.json";//获取主页面新闻中心内容分类信息
 }
