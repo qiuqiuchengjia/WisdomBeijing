bf9ec996ab44fb9b4d0e237110ac8c22dc223d0e
qiuchengjia <qiuqiuchengjia@163.com>
2016-08-24 21:33:46 +0800

    重写ViewPager的dispatchTouchEvent方法，优化ViewPager的滑动事件，使得向右滑动的时候不会调出sldingMenu

---
+++ app/src/main/java/com/qiu/View/HorizonabViewPager.java
@@ -0,0 +1,34 @@
+package com.qiu.View;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Description: 新闻中心中的新闻下的子页签水平滑动的ViewPager
+ * Data：2016/8/24-21:25
+ * Blog：www.qiuchengjia.cn
+ * Author: qiu
+ */
+public class HorizonabViewPager extends ViewPager {
+    /**
+     * 事件分发,请求父控件及祖宗控件不要拦截事件
+     * @param ev the ev
+     * @return the boolean
+     * @author qiu  博客：www.qiuchengjia.cn 时间：2016-08-24
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        getParent().requestDisallowInterceptTouchEvent(true);//用getParent去请求
+        return super.dispatchTouchEvent(ev);
+    }
+
+    public HorizonabViewPager(Context context) {
+        super(context);
+    }
+
+    public HorizonabViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
