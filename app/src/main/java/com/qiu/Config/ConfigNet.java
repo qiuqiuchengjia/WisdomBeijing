6bd7c0e0542c4c9dfece8962db0958046de2f891
qiuchengjia <qiuqiuchengjia@163.com>
2016-08-25 16:52:06 +0800

    修改测试服务器为github

--- app/src/main/java/com/qiu/Config/ConfigNet.java
+++ app/src/main/java/com/qiu/Config/ConfigNet.java
@@ -7,6 +7,7 @@ package com.qiu.Config;
  * 说明：网络config，用来存放与网络有关的配置
  */
 public class ConfigNet {
-    public static final String SERVER_URL="http://59.71.5.15";//服务器的URL
-    public static final String CATEGORIES_URL=SERVER_URL+"/qiu/WisdomBeijingServer/categories.json";//获取主页面新闻中心内容分类信息
+    public static final String SERVER_URL="https://qiuqiuchengjia.github.io/";//服务器的URL
+    public static final String SERVER_FILE_NAME="WisdomBeijing/";//智慧北京服务器的文件夹
+    public static final String CATEGORIES_URL=SERVER_URL+SERVER_FILE_NAME+"categories.json";//获取主页面新闻中心内容分类信息
 }
